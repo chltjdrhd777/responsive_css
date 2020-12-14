10f1ec146b80d0144164547455b92d6524d4ee14
unknown <chltjdrhd777@naver.com>
2020-12-14 17:42:27 +0900

    done

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "scss/Header.scss";
+import { Link } from "@material-ui/core";
+import "scss/Header.scss";
+
+function Header() {
+  return (
+    <div className="navBar">
+      <div className="container">
+        <h1 className="logo">Anderson</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/Features">Features</Link>
+            </li>
+            <li>
+              <Link href="/Docs">Docs</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
